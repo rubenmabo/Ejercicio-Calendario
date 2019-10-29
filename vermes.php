1f4037612b1dc44371e4d710a91b889c756a14fa
rubenmabo <47483262+rubenmabo@users.noreply.github.com>
2019-10-29 11:51:01 +0100

    Add files via upload

---
+++ vermes.php
@@ -0,0 +1,60 @@
+<?php
+
+$MES=FEBRERO;
+$ANIO=2019;
+cont =1;
+for($num=1; $num<36; $num++){
+    if
+    $n[]=num;
+   
+}
+
+
+
+//Parte de tabla, parte grafica
+echo "<table border=1>";
+    echo "<tr>";
+    echo "<td colspan='7'> MES: " .$MES. "    -    " .$ANIO. "</td>";
+    echo "</tr>";
+    echo "<tr>";
+        echo "<td>" .L. "</td>";
+        echo "<td>" .M. "</td>";
+        echo "<td>" .X. "</td>";
+        echo "<td>" .J. "</td>";
+        echo "<td>" .V. "</td>";
+        echo "<td>" .S. "</td>";
+        echo "<td>" .D. "</td>";
+    echo "</tr>";
+    echo "<tr>";
+        echo "<td>" .$n1. "</td>";
+        echo "<td>" .$n2. "</td>";
+        echo "<td>" .$n3. "</td>";
+        echo "<td>" .$n4. "</td>";
+        echo "<td>" .$n5. "</td>";
+        echo "<td>" .$n6. "</td>";
+        echo "<td>" .$n7. "</td>";
+    echo "</tr>";
+    echo "<tr>";
+        echo "<td>" .$n8. "</td>";
+        echo "<td>" .$n9. "</td>";
+        echo "<td>" .$n10. "</td>";
+        echo "<td>" .$n11. "</td>";
+        echo "<td>" .$n12. "</td>";
+        echo "<td>" .$n13. "</td>";
+        echo "<td>" .$n14. "</td>";
+    echo "</tr>";
+    echo "<tr>";
+        echo "<td>" .$n15. "</td>";
+        echo "<td>" .$n16. "</td>";
+        echo "<td>" .$n17. "</td>";
+        echo "<td>" .$n18. "</td>";
+        echo "<td>" .$n19. "</td>";
+        echo "<td>" .$n20. "</td>";
+        echo "<td>" .$n21. "</td>";
+    echo "</tr>";
+echo "</table>";
+
+?>
+<hr>
+<?php show_source(__FILE__); ?>
+<hr>
